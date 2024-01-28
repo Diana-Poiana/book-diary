f73569bb588052f92e5acd771677c4425ccf860f
Diana Ivanova <diana.website.developer@gmail.com>
2024-01-28 13:07:08 +0000

    local storage nearly done

---
+++ js/firebaseConfiguration.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getDatabase, get, set, ref, child } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGBV9s_-L23g0rCM9N48IvZVuhRaWMyX4",
+  authDomain: "book-review-notebook.firebaseapp.com",
+  projectId: "book-review-notebook",
+  storageBucket: "book-review-notebook.appspot.com",
+  messagingSenderId: "240784539463",
+  appId: "1:240784539463:web:1e54be88165d676a5c1662",
+  measurementId: "G-J66LNVCTVR"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth(app);
+const dbref = ref(db);
+
+export { app, db, auth, set, get, ref, dbref, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, child };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
+import { db, ref, dbref, set, get, auth, child, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification } from './firebaseConfiguration.js';
+
 window.addEventListener('DOMContentLoaded', () => {
+
+  // authentication
+  const formSignUp = document.querySelector('.form__sign-up');
+  const formLogIn = document.querySelector('.form__log-in');
+  const emailInput = document.getElementById('email');
+  const phoneInput = document.getElementById('phone');
+  const passwordInput = document.getElementById('password');
+
+  const hidePasswordBtn = document.querySelector('.form__hide-button');
+  const btnTextInner = document.querySelector('.form__btn-txt');
+  const showSvg = document.querySelector('.form__show-svg');
+  const hideSvg = document.querySelector('.form__hide-svg');
+
+  const logInBtntoHide = document.querySelector('.list-of-books__autorization-btn');
+  const userTextToShowAfterAutho = document.querySelector('.list-of-books__authorization-done');
+
+  const signUpBtn = document.querySelector('.form__button');
+  const loader = document.querySelector('.form__loader');
+
   // rating stars
   const settingRating = document.getElementById('setting');
   const plotRating = document.getElementById('plot');
@@ -17,6 +38,98 @@ window.addEventListener('DOMContentLoaded', () => {
   // img input
   const bookCoverInput = document.querySelector('.book-description__cover-input');
   const bookCover = document.querySelector('.book-description__cover-img');
+
+  // calendar
+
+  const currentDate = new Date();
+  const maxDate = currentDate.toISOString().split('T')[0];
+  currentDate.setDate(currentDate.getDate() - 7);
+  const minDate = currentDate.toISOString().split('T')[0];
+
+  // local storage
+
+  const allUserInputs = document.querySelectorAll('.item__user-input');
+  const saveBtn = document.querySelector('.review__save-btn');
+  const userData = JSON.parse(localStorage.getItem('userData'));
+
+  // local storage collecting data
+
+  function collectAllUserData() {
+    allUserInputs.forEach(input => {
+      let dataAttribute = input.getAttribute('name');
+
+      input.addEventListener('input', function () {
+        userData[dataAttribute] = input.innerText;
+        updateLocalStorage();
+      })
+    });
+  }
+
+  // local storage updating data
+
+  function updateLocalStorage() {
+    localStorage.setItem('userData', JSON.stringify(userData));
+  }
+
+  function applyUserData() {
+    allUserInputs.forEach(input => {
+      let dataAttribute = input.getAttribute('name');
+
+      if (userData[dataAttribute] === '') {
+        input.innerText = '...';
+      } else {
+        input.innerText = userData[dataAttribute];
+      }
+    })
+  }
+
+  collectAllUserData();
+  applyUserData();
+
+  // send all data to firebase
+  function saveAllDataAndSendToFirebase() {
+    const userInfoString = sessionStorage.getItem('user-creds');
+    const userInfo = JSON.parse(userInfoString);
+
+    if (userInfo && userInfo.uid) {
+      const userID = userInfo.uid;
+
+      // Ensure that userData is defined and contains the necessary data
+      if (userData) {
+        // Assuming userData is an object with properties you want to save
+        set(ref(db, 'users/' + userID), userData)
+          .then(() => {
+            console.log('Данные успешно отправлены в Firebase!');
+          })
+          .catch((error) => {
+            console.error('Ошибка при отправке данных в Firebase:', error);
+          });
+      } else {
+        console.error('Ошибка: userData не определен.');
+      }
+    } else {
+      console.error('Ошибка: Невозможно получить информацию о пользователе из sessionStorage.');
+    }
+  }
+
+
+
+
+
+  // const startDate = datepicker('.book-description__start-date', {
+  //   minDate: new Date(minDate),
+  //   formatter: (input, date) => {
+  //     const formattedDate = date.toLocaleDateString();
+  //     input.value = formattedDate;
+  //   }
+  // });
+
+  // const finishDate = datepicker('.book-description__finish-date', {
+  //   maxDate: new Date(maxDate), formatter: (input, date) => {
+  //     const formattedDate = date.toLocaleDateString();
+  //     input.value = formattedDate;
+  //   }
+  // });
 
   // rating stars
   function getRating(inputs, startsVariable) {
@@ -40,7 +153,7 @@ window.addEventListener('DOMContentLoaded', () => {
       overallStars = (settingStars + plotStars + charactersStars + styleStars + engagementStars) / 5;
     }
 
-    result = Math.round(overallStars * 2) / 2;
+    let result = Math.round(overallStars * 2) / 2;
     const overallInputs = Array.from(overallRating.children);
     overallInputs.forEach((elem) => {
       if (+elem.value === result) {
@@ -49,6 +162,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  // book cover upload
   function loadBookCover() {
     let userCover = bookCoverInput.files[0];
     bookCover.src = URL.createObjectURL(userCover);
@@ -57,15 +171,176 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // bookCover.src = localStorage.getItem('bookCover');
 
-  // event listeners
-  settingRating.addEventListener('change', changeOverallRating);
-  plotRating.addEventListener('change', changeOverallRating);
-  charactersRating.addEventListener('change', changeOverallRating);
-  styleRating.addEventListener('change', changeOverallRating);
-  engagementRating.addEventListener('change', changeOverallRating);
 
-  bookCoverInput.addEventListener('change', loadBookCover);
-  bookCover.addEventListener('click', () => {
-    bookCoverInput.click();
-  });
+
+  // firebase
+
+
+  if (sessionStorage.getItem('user-creds')) {
+    if (logInBtntoHide && userTextToShowAfterAutho) {
+      logInBtntoHide.style.display = 'none';
+      const userInfoString = sessionStorage.getItem('user-creds');
+      const userInfo = JSON.parse(userInfoString);
+      const userEmail = userInfo.email;
+      const userEmailToShow = userEmail.split('@');
+      userTextToShowAfterAutho.textContent = `Hello, ${userEmailToShow[0]}`;
+    }
+  }
+
+  //create a new user
+  function createNewUser(e) {
+    e.preventDefault();
+    showLoader();
+
+    createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
+      .then((credentials) => {
+        set(ref(db, 'UsersAuthList/' + credentials.user.uid), {
+          phone: phoneInput.value
+        });
+
+        sendEmailVerification(auth.currentUser)
+          .catch((error) => {
+            console.error('Error occured:', error);
+          });
+
+        sessionStorage.setItem('user-creds', JSON.stringify(credentials.user));
+        window.location.href = 'index.html';
+
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }
+
+
+
+  // sign in existing user
+  function loginUser(e) {
+    e.preventDefault();
+    showLoader();
+
+    signInWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
+      .then((credentials) => {
+        get(child(dbref, 'UsersAuthList/' + credentials.user.uid))
+          .then((snapshot) => {
+            if (snapshot.exists) {
+              sessionStorage.setItem('user-info', JSON.stringify({
+                phone: snapshot.val().phone
+              }));
+              sessionStorage.setItem('user-creds', JSON.stringify(credentials.user));
+
+              window.location.href = 'index.html';
+
+
+            }
+          });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }
+
+  // toggle password visabillity
+  function togglePasswordVisability() {
+    if (passwordInput.getAttribute('type') == 'password') {
+      passwordInput.removeAttribute('type');
+      passwordInput.setAttribute('type', 'text');
+      btnTextInner.textContent = 'Show';
+      hideSvg.style.display = 'none';
+      showSvg.style.display = 'block';
+    } else if (passwordInput.getAttribute('type') == 'text') {
+      passwordInput.removeAttribute('type');
+      passwordInput.setAttribute('type', 'password');
+      btnTextInner.textContent = 'Hide';
+      hideSvg.style.display = 'block';
+      showSvg.style.display = 'none';
+    }
+  }
+
+  //loader
+  function showLoader() {
+    signUpBtn.setAttribute('disabled', true);
+    loader.style.display = 'flex';
+  }
+
+
+
+  // event listeners
+  try {
+    settingRating.addEventListener('change', changeOverallRating);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    plotRating.addEventListener('change', changeOverallRating);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    charactersRating.addEventListener('change', changeOverallRating);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    styleRating.addEventListener('change', changeOverallRating);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    bookCoverInput.addEventListener('change', loadBookCover);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    engagementRating.addEventListener('change', changeOverallRating);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    bookCoverInput.addEventListener('change', loadBookCover);
+    bookCover.addEventListener('click', () => {
+      bookCoverInput.click();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+
+
+
+
+  try {
+    hidePasswordBtn.addEventListener('click', togglePasswordVisability);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    formLogIn.addEventListener('submit', loginUser);
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    formSignUp.addEventListener('submit', createNewUser);
+  } catch (error) {
+    console.log(error);
+  }
+
+
+
+  try {
+    saveBtn.addEventListener('click', () => {
+      saveAllDataAndSendToFirebase();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
 });
